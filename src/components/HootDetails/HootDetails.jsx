914a0b3d4d1a167f019f96c9352c68761b8e0bee
tsztin0217 <tsztin0217@gmail.com>
2025-04-01 17:50:20 -0400

    completed handleAddComment helper function

--- src/components/HootDetails/HootDetails.jsx
+++ src/components/HootDetails/HootDetails.jsx
@@ -2,10 +2,23 @@ import { useState, useEffect } from 'react';
 import * as hootService from '../../services/hootService';
 // import { show } from '../../services/hootService'; // best way to import
 import { useParams } from 'react-router';
+import CommentForm from '../CommentForm/CommentForm';
+
 
 const HootDetails = (props) => {
     const [hoot, setHoot] = useState(null);
     const { hootId } = useParams();
+
+    // src/components/HootDetails/HootDetails.jsx
+
+    // src/components/HootDetails/HootDetails.jsx
+
+  const handleAddComment = async (commentFormData) => {
+    const newComment = await hootService.createComment(hootId, commentFormData);
+    setHoot({ ...hoot, comments: [...hoot.comments, newComment] });
+  };
+
+
 
     useEffect(() => {
         const fetchHoot = async () => {
@@ -26,33 +39,38 @@ const HootDetails = (props) => {
                     <p>{hoot.category.toUpperCase()}</p>
                     <h1>{hoot.title}</h1>
                     <p>
-                        {`${hoot.author.username} posted on
-                          ${new Date(hoot.createdAt).toLocalDateString()}`}
+                        {`${hoot.author.username} posted on 
+                          ${new Date(hoot.createdAt).toLocaleDateString()}`}
                     </p>
                 </header>
                 <p>{hoot.text}</p>
             </section>
-            
+
+
+
             // src/components/HootDetails/HootDetails.jsx
 
-{/* All updates are in the comments section! */}
-<section>
-  <h2>Comments</h2>
+            <section>
+                <h2>Comments</h2>
+                {/* Pass the handleAddComment function to the CommentForm Component */}
+                <CommentForm handleAddComment={handleAddComment} />
 
-  {!hoot.comments.length && <p>There are no comments.</p>}
+                {!hoot.comments.length && <p>There are no comments.</p>}
 
-  {hoot.comments.map((comment) => (
-    <article key={comment._id}>
-      <header>
-        <p>
-          {`${comment.author.username} posted on
-          ${new Date(comment.createdAt).toLocaleDateString()}`}
-        </p>
-      </header>
-      <p>{comment.text}</p>
-    </article>
-  ))}
-</section>
+                {hoot.comments.map((comment) => (
+                    <article key={comment._id}>
+                        <header>
+                            <p>
+                                {`${comment.author.username} posted on
+                ${new Date(comment.createdAt).toLocaleDateString()}`}
+                            </p>
+                        </header>
+                        <p>{comment.text}</p>
+                    </article>
+                ))}
+            </section>
+
+
 
         </main>
     );
