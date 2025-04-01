646cd43d5ccf4149ed0490790c67e02cb49e0585
tsztin0217 <tsztin0217@gmail.com>
2025-04-01 17:49:12 -0400

    called helper function from handleSubmit handler

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -3,32 +3,34 @@
 import { useState } from 'react';
 
 const CommentForm = (props) => {
-  const [formData, setFormData] = useState({ text: '' });
+    const [formData, setFormData] = useState({ text: '' });
 
-  const handleChange = (evt) => {
-    setFormData({ ...formData, [evt.target.name]: evt.target.value });
-  };
+    const handleChange = (evt) => {
+        setFormData({ ...formData, [evt.target.name]: evt.target.value });
+    };
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    // add handleAddComment
-    setFormData({ text: '' });
-  };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor='text-input'>Your comment:</label>
-      <textarea
-        required
-        type='text'
-        name='text'
-        id='text-input'
-        value={formData.text}
-        onChange={handleChange}
-      />
-      <button type='submit'>SUBMIT COMMENT</button>
-    </form>
-  );
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        props.handleAddComment(formData);
+        setFormData({ text: '' });
+    };
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='text-input'>Your comment:</label>
+            <textarea
+                required
+                type='text'
+                name='text'
+                id='text-input'
+                value={formData.text}
+                onChange={handleChange}
+            />
+            <button type='submit'>SUBMIT COMMENT</button>
+        </form>
+    );
 };
 
 export default CommentForm;
