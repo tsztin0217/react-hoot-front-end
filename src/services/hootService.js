66df0348cf2d7cc541e05a4d55854018d572122c
tsztin0217 <tsztin0217@gmail.com>
2025-04-01 17:48:19 -0400

    Add create comment serve function

--- src/services/hootService.js
+++ src/services/hootService.js
@@ -13,4 +13,60 @@ const index = async () => {
     }
 }
 
-export { index }; // named export syntax (used to export multiple function from a module)
+
+
+const show = async (hootId) => {
+    try {
+        const res = await fetch(`${BASE_URL}/${hootId}`, {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        });
+        return res.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+const create = async (hootFormData) => {
+    try {
+        const res = await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(hootFormData)
+        });
+        return res.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+// src/services/hootService.js
+
+const createComment = async (hootId, commentFormData) => {
+    try {
+        const res = await fetch(`${BASE_URL}/${hootId}/comments`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(commentFormData),
+        });
+        return res.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export {
+    index,
+    show,
+    create,
+    createComment,
+};
+
+
