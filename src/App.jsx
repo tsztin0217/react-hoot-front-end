7ab1d0f1fdeb58b9a0d8248d70816599b07b6f74
tsztin0217 <tsztin0217@gmail.com>
2025-04-01 17:50:39 -0400

    adds most recent changes after lunch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import { Routes, Route } from 'react-router';
+import { useContext, useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router';
 
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
@@ -7,27 +7,72 @@ import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import HootList from './components/HootList/HootList';
+import HootDetails from './components/HootDetails/HootDetails';
+import HootForm from './components/HootForm/HootForm';
+
 
 import { UserContext } from './contexts/UserContext';
 
+import * as hootService from './services/hootService';
+
 const App = () => {
   const { user } = useContext(UserContext);
-  
+  const [hoots, setHoots] = useState([]);
+  const navigate = useNavigate();
+
+
+const handleAddHoot = async (hootFormData) => {
+  const newHoot = await hootService.create(hootFormData);
+  setHoots([newHoot, ...hoots]);
+  navigate('/hoots');
+};
+
+
+
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+
+      // console.log('hootsData: ', hootsData);
+      setHoots(hootsData); // setting hoots state
+    };
+
+    if (user) fetchAllHoots();
+    // ^ only fetch hoots when a user is logged in
+  }, [user]); // adding user dependency
+  // because the effect depends on the user to run
+
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <Routes>
         <Route path='/' element={user ? <Dashboard /> : <Landing />} />
-        {
-          user ? (
-            <Route ppath="/hoots" element={<HootList />} />
-          ) : (
-            <>
-              <Route path='/sign-up' element={<SignUpForm />} />
-              <Route path='/sign-in' element={<SignInForm />} />
-            </>
-          )
-        }
+        // src/App.jsx
+
+        {user ? (
+          <>
+            {/* Protected Routes (available only to signed-in users) */}
+            <Route path='/hoots' element={<HootList hoots={hoots} />} />
+            // src/App.jsx
+
+            <Route
+              path='/hoots/new'
+              element={<HootForm handleAddHoot={handleAddHoot} />}
+            />
+
+            <Route
+              path='/hoots/:hootId'
+              element={<HootDetails />}
+            />
+          </>
+        ) : (
+          <>
+            {/* Non-user Routes (available only to guests) */}
+            <Route path='/sign-up' element={<SignUpForm />} />
+            <Route path='/sign-in' element={<SignInForm />} />
+          </>
+        )}
+
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path='/sign-in' element={<SignInForm />} />
       </Routes>
